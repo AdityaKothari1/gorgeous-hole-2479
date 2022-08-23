8d3c4f5a24b7b7c7cc24167d948571f50b609cf2
AdityaKothari1 <105914134+AdityaKothari1@users.noreply.github.com>
2022-08-23 22:58:03 +0530

    day1

---
+++ sephoraclone/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './Components/Footer';
+import MakeUp from './Components/MakeUp';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Footer/>
+      <MakeUp />
+    </div>
+  );
+}
+
+export default App;
